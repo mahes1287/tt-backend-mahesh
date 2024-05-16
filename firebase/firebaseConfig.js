724ba5d1f43a8bd93ab427323116a85381af68b9
Maheshwaran Velusamy <mahes1287@gmail.com>
2024-05-16 15:53:34 +0200

    first commit

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+// const db = getFirestore(app);
+
+// export default app;
+
+const db = getFirestore();
+
+export default db;
---
+++ server.js
@@ -0,0 +1,78 @@
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+import cors from '@fastify/cors'
+
+
+import axios from "axios";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+// import app from "./firebase/firebaseConfig.js";
+// import "./firebase/firebaseConfig.js";
+import db from "./firebase/firebaseConfig.js";
+// const db = getFirestore(app);
+import { doc, setDoc } from "firebase/firestore";
+await fastify.register(cors, { 
+  // put your options here
+})
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  try {
+    const querySnapshot = await getDocs(collection(db, "pokemons"));
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      data.push(doc.data());
+    });
+    console.log(data);
+    return data;
+  } catch (error) {
+    fastify.log.error(error);
+    return reply
+      .code(500)
+      .send({ error: "Failed to fetch data from local server" });
+  }
+});
+async function loadData(request, reply) {
+  try {
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=10"
+    );
+    const pokemons = response.data.results;
+
+    const detailedPokemons = await Promise.all(
+      pokemons.map(async (pokemon) => {
+        const details = await axios.get(pokemon.url);
+        return {
+          name: details.data.name,
+          abilities: details.data.abilities.map(
+            (ability) => ability.ability.name
+          ),
+          image: details.data.sprites.front_default,
+        };
+      })
+    );
+
+    detailedPokemons.map(async (pokemon) => {
+      await setDoc(doc(db, "pokemons", pokemon.name), {
+        ...pokemon,
+      });
+    });
+    return detailedPokemons;
+  } catch (error) {
+    fastify.log.error(error);
+    return reply
+      .code(500)
+      .send({ error: "Failed to fetch data from Pokemon API" });
+  }
+}
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+  await loadData();
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
